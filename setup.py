a86bafbe40d73091da9ddfafed9e52886ef69430
Whit Morriss <whit@surveymonkey.com>
2012-02-15 14:10:03 -0600

    fix up ol'setup.py

--- setup.py
+++ setup.py
@@ -3,17 +3,18 @@ from setuptools import setup
 
 version = '0.0a1'
 
-requires = ['pyramid', 'pyramid_jinja2']
+requires = ['pyramid', 
+            'pyramid_jinja2']
 
 setup(name='Prism',
       version=version,
-      description="A simple framework atop pyramid",
+      description="A simple plug driven framework atop pyramid",
       long_description=open('README.rst').read(),
       classifiers=[], 
       keywords='',
       author='',
-      author_email='',
-      url='',
+      author_email='whit at surveymonkey.com',
+      url='http://prism.github.com',
       license='BSD',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
