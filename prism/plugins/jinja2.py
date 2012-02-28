11f0b00782944bccb20a8d0f6ce762588f1d7a25
Whit Morriss <whit@surveymonkey.com>
2012-02-27 17:26:36 -0800

    add filters

--- prism/plugins/jinja2.py
+++ prism/plugins/jinja2.py
@@ -1,8 +1,14 @@
 from pyramid_jinja2 import renderer_factory
+from pyramid_jinja2 import parse_filters
 
 
 def includeme(config):
-    config.include('pyramid_jinja2')
     config.add_renderer('.html', renderer_factory)
-    tmplt_dirs = config.settings['jinja2.directories']
-    config.add_jinja2_search_path(tmplt_dirs)
+    tmplt_dirs = config.settings.get('jinja2.directories')
+    if tmplt_dirs:
+        config.add_jinja2_search_path(tmplt_dirs)
+    filters = config.settings.get('jinja2.filters')
+    if filters:
+        filters = parse_filters(filters)
+        environment = config.get_jinja2_environment()
+        environment.filters.update(filters)
