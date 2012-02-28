decae4d013cc6085bab5fdf237acc714b2f07ce7
Whit Morriss <whit@surveymonkey.com>
2012-02-27 16:05:18 -0800

    jinja2 dirs

--- prism/plugins/jinja2.py
+++ prism/plugins/jinja2.py
@@ -4,3 +4,5 @@ from pyramid_jinja2 import renderer_factory
 def includeme(config):
     config.include('pyramid_jinja2')
     config.add_renderer('.html', renderer_factory)
+    tmplt_dirs = config.settings['jinja2.directories']
+    config.add_jinja2_search_path(tmplt_dirs)
